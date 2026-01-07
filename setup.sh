9194887e134c8b02a92fea62c4da2e8308403f1b
Edwin Campos <ecamposc97@gmail.com>
2026-01-06 22:55:46 -0500

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+#
+mkdir -p "$HOME/.local/bin" "$HOME/.local/etc"
+sudo chmod 744 *.sh
+
+cp *.sh "$HOME/.local/bin/"
+cp .env "$HOME/.local/etc/"
+
+cp slides.desktop "$HOME/.config/autostart/"
+sed -i "s|HOME|$HOME|g" "$HOME/.config/autostart/slides.desktop"
--- slides.sh
+++ slides.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source .env
+source "$1"
 cd "$PLAYLIST_DIR" || exit 1
 mpv --loop-playlist --input-ipc-server=/tmp/mpvsocket --use-filedir-conf --idle
