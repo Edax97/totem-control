0190aa6f8b12ea582ee10c82c2855df00c456c47
Edwin Campos <ecamposc97@gmail.com>
2026-01-07 01:25:01 -0500

    A

--- setup.sh
+++ setup.sh
@@ -5,11 +5,11 @@ mkdir -p "$APP_DIR" "$HOME/.config/autostart"
 
 sudo chmod 744 app/*.sh
 
-cp ./app/*.sh "$APP/"
+cp ./app/*.sh "$APP_DIR/"
 cp .env "$HOME/.env"
 cp .settings.yaml "$HOME/Pictures/"
 
-cp slides.desktop init-ftp.desktop config-manager.desktop "$HOME/.config/autostart/"
+cp app/slides.desktop app/init-ftp.desktop app/config-manager.desktop "$HOME/.config/autostart/"
 sed -i "s|APP|$APP_DIR|g" "$HOME/.config/autostart/slides.desktop"
 sed -i "s|APP|$APP_DIR|g" "$HOME/.config/autostart/init-ftp.desktop"
 sed -i "s|APP|$APP_DIR|g" "$HOME/.config/autostart/config-manager.desktop"
