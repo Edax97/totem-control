a41794bbca0d2578f652d1cb2ff644c433c1a3c6
Edwin Campos <ecamposc97@gmail.com>
2026-01-20 00:03:32 -0500

    Copyparty sh updated acam

--- copyparty/copyparty.sh
+++ copyparty/copyparty.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /usr/local/etc/.env
-/usr/bin/python /usr/local/bin/copyparty-sfx.py -v "$CONTENT_DIR:/content:r:rwd,adm" -a adm:labotec --rproxy=1 --rp-loc /copyparty --xff-src=100.0.0.0/8,127.0.0.1 --acao https://telepresencia-manager-ui.vercel.app/ --acam "GET,HEAD,PUT,POST,DELETE,UPDATE,OPTIONS"
+/usr/bin/python /usr/local/bin/copyparty-sfx.py -v "$CONTENT_DIR:/content:r:rwd,adm" -a adm:labotec --rproxy=1 --rp-loc /copyparty --xff-src=100.0.0.0/8,127.0.0.1 --acao https://telepresencia-manager-ui.vercel.app/ --acam "*"
