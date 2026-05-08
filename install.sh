0e56fc1dba9e0661fc1d99a772713a3c316cb3a1
Edwin Campos <ecamposc97@gmail.com>
2026-05-08 12:36:23 -0500

    fix mediamtx

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
 sudo apt install mpv socat ffmpeg yq
 sudo apt install nginx
 sudo systemctl enable --now nginx
+
+#disable firefox crash recovery
+# -- about:config
+# browser.sessionstore.resume_from_crash False
+# browser.sessionstore.max_resumed_crashes -1
+
+# install jitsi-meet
+# 
